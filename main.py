60f09764d13e46f950eb2325ab6a82d728d035cf
nharris710 <noaharris248@gmail.com>
2023-02-20 01:35:42 -0800

    Celsius

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+
+    C = int(input("Enter Celsius:"))
+    F = 9 / 5 * C + 32
+    print("Fahrenheit is: {0:.2f}".format(F))
+    # print(f"Fahrenheit is: {F:.2f}")  
 
 
 if __name__ == '__main__':
